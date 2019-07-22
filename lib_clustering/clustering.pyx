4cf2a5becc0070f58287be5c01471cc7a8efc47f
Daniel <exodaniel@gmail.com>
2019-07-22 17:47:42 +0200

    clustering functional at 500Hz

--- lib_clustering/clustering.pyx
+++ lib_clustering/clustering.pyx
@@ -17,7 +17,7 @@ import os
 from yaml import load
 from matplotlib.pyplot import imread
 
-def ranges_to_xy(np.float32_t[:] ranges, np.float32_t[:] angles,
+cdef cranges_to_xy(np.float32_t[:] ranges, np.float32_t[:] angles,
                  np.float32_t[:] x, np.float32_t[:] y):
     for i in range(ranges.shape[0]):
         x[i] = ranges[i] * ccos(angles[i])
@@ -25,31 +25,60 @@ def ranges_to_xy(np.float32_t[:] ranges, np.float32_t[:] angles,
 
 
 
-def euclidean_clustering(np.float32_t[:] ranges):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
+def euclidean_clustering(np.float32_t[:] ranges, thresh=0.5):
     # assume angles from 0 to 2pi
-    cdef np.float32_t[:] angles = np.linspace(0, 2*np.pi, ranges.shape[0]+1)[:-1]
+    cdef int n_points = ranges.shape[0]
+    cdef np.float32_t[:] angles = np.linspace(0, 2*np.pi, ranges.shape[0]+1, dtype=np.float32)[:-1]
     cdef np.float32_t[:] x = np.zeros((n_points), dtype=np.float32)
     cdef np.float32_t[:] y = np.zeros((n_points), dtype=np.float32)
-    ranges_to_xy(ranges, angles, x, y)
+    cranges_to_xy(ranges, angles, x, y)
 
-    THRESH = 0.1
-    THRESH_SQ = THRESH * THRESH
+    cdef np.float32_t THRESH = thresh
+    cdef np.float32_t THRESH_SQ = THRESH * THRESH
 
     clusters = []
     cdef bool create_new_cluster = True
-    for i in range(x.shape[0]):
+    cdef np.float32_t xi
+    cdef np.float32_t yi
+    cdef np.float32_t dx
+    cdef np.float32_t dy
+    for i in range(n_points):
         create_new_cluster = True
         xi = x[i]
         yi = y[i]
-        for c in clusters:
-            for p in c:
+        for j in range(len(clusters)):
+            c = clusters[j]
+            for k in range(len(c)):
+                p = c[k]
                 dx = xi - x[p]
                 dy = yi - y[p]
                 if ( dx * dx + dy * dy ) < THRESH_SQ:
-                    c.append(p)
+                    c.append(i)
                     create_new_cluster = False
+                    break
+            if not create_new_cluster:
+                break
         if create_new_cluster:
             clusters.append([i])
 
-    return clusters
+    return clusters, x, y
+
+def cluster_ids(n_points, clusters):
+    max_id = np.max(np.max(clusters))
+    if max_id > n_points:
+        raise ValueError("n_points ({}) smaller than cluster point ids ({}) for clusters {}".format(
+            n_points, max_id, clusters))
+    cdef np.int64_t[:] cluster_ids = np.zeros((n_points), dtype=np.int64)
+
+    for i in range(len(clusters)):
+        c = clusters[i]
+        for p in c:
+            cluster_ids[int(p)] = i
+
+    return cluster_ids
+
 
