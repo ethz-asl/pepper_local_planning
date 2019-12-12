61ab0baa5b0592b6e9db6881d680675b162b521d
Daniel <exodaniel@gmail.com>
2019-12-12 15:56:49 +0100

    follows global_planner waypoint instead of having own waypoint paradigm

--- scripts/responsive.py
+++ scripts/responsive.py
@@ -24,7 +24,7 @@ class Responsive(object):
         self.kLidarTopic = "/combined_scan"
         self.kFixedFrameTopic = "/pepper_robot/odom"
         self.kCmdVelTopic = "/cmd_vel"
-        self.kGlobalPathTopic = "/global_planner/global_path"
+        self.kGlobalWaypointTopic = "/global_planner/current_waypoint"
         self.kFixedFrame = "odom" # ideally something truly fixed, like /map
         self.kRobotFrame = "base_footprint"
         self.kMaxObstacleVel_ms = 10. # [m/s]
@@ -40,10 +40,9 @@ class Responsive(object):
         if args.no_stop:
             self.STOP = False
         self.is_tracking_global_path = False
-        self.waypoint_dtg = None # DTG of current waypoint along global path. can only decrease if path is constant.
         # ROS
         rospy.init_node('responsive', anonymous=True)
-        rospy.Subscriber(self.kGlobalPathTopic, Path, self.global_path_callback, queue_size=1)
+        rospy.Subscriber(self.kGlobalWaypointTopic, Marker, self.global_waypoint_callback, queue_size=1)
         rospy.Subscriber(self.kLidarTopic, LaserScan, self.scan_callback, queue_size=1)
         rospy.Subscriber(self.kFixedFrameTopic, Odometry, self.odom_callback, queue_size=1)
         self.pubs = [rospy.Publisher("debug{}".format(i), LaserScan, queue_size=1) for i in range(3)]
@@ -53,7 +52,6 @@ class Responsive(object):
         self.tf_br = tf.TransformBroadcaster()
         # Timers
         rospy.Timer(rospy.Duration(0.001), self.tf_callback)
-        rospy.Timer(rospy.Duration(0.1), self.global_path_tracking_routine)
         # Services
         rospy.Service('stop_autonomous_motion', Trigger, 
                 self.stop_autonomous_motion_service_call)
@@ -83,114 +81,29 @@ class Responsive(object):
                              "goal",
                              self.kFixedFrame)
 
-    def global_path_callback(self, msg):
+    def global_waypoint_callback(self, msg):
         """ If a global path is received (in map frame), try to track it """
         with self.lock:
-            self.global_path = msg
-            self.is_tracking_global_path = True
-        # TODO: if new path is longer, update the progress
-        # how far is the robot current pos from goal along old path? (can be unclear as we might be off path)
-        # how far is the robot current pos from goal along new path? (easy)
-        # difference between the two is the path elongation/shortening
-        # ...
-
-    def global_path_tracking_routine(self, event):
-        """ sets the goal to a waypoint along the global path """
-        with self.lock:
-            self.WAYPOINT_DIST_STEPS = 5 # steps between closest point on path and waypoint. should translate to ~1-2m real distance. assumes evenly sampled path.
-    #         self.WAYPOINT_DIST_M = 1.
-            if not self.is_tracking_global_path:
-                return
-            ## transform path to fixed frame
-            try:
-                 tf_path_in_fix = self.tf_listener.lookupTransform(
-                         self.kFixedFrame,
-                         self.global_path.header.frame_id,
-                         self.global_path.header.stamp - rospy.Duration(0.1))
-                 tf_rob_in_fix = self.tf_listener.lookupTransform(self.kFixedFrame, self.kRobotFrame, rospy.Time(0))
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-                print("Could not set goal from global path:")
-                print(e)
-                return
-            # path in path frame
-            gp_xy = np.array(
-                    [[pose_st.pose.position.x, pose_st.pose.position.y]
-                      for pose_st in self.global_path.poses]
-                    )
-            # path in fixed frame
-            gp_fix_xy = apply_tf(gp_xy, Pose2D(tf_path_in_fix))
-            # distance to goal along path
-            gp_dtg = np.array([pose_st.pose.position.z for pose_st in self.global_path.poses])
-            ## robot xy in fixed frame
-            rob_fix_xy =  Pose2D(tf_rob_in_fix)[:2]
-            ## check if goal has been reached
-            if np.linalg.norm(rob_fix_xy - gp_fix_xy[-1]) < self.kRobotRadius_m:
-                print("global_goal reached")
-                # set goal as global goal and stop tracking
-                self.is_tracking_global_path = False
-                self.waypoint_dtg = None
-                wpt_fix_xy = gp_fix_xy[-1]
-                self.tf_goal_in_fix = (np.array([wpt_fix_xy[0], wpt_fix_xy[1], 0.]), # trans
-                                       tf.transformations.quaternion_from_euler(0,0,0)) # quat
-                return
-            ## if waypoint is not set yet, set waypoint one meter from the robot along the path
-            if self.waypoint_dtg is None:
-                # walk N steps forwards from start of path
-                waypoint_id = min(len(gp_dtg)-1, self.WAYPOINT_DIST_STEPS)
-                self.waypoint_dtg = gp_dtg[waypoint_id]
-                # set waypoint 
-                wpt_fix_xy = gp_fix_xy[waypoint_id]
-                self.tf_goal_in_fix = (np.array([wpt_fix_xy[0], wpt_fix_xy[1], 0.]), # trans
-                                       tf.transformations.quaternion_from_euler(0,0,0)) # quat
-                return
-            ## check if we have progressed
-            # find closest point to robot along the path
-            distances = np.linalg.norm(rob_fix_xy - gp_fix_xy, axis=-1)
-            closest_dist = np.min(distances)
-            # adds slack in case path has weird shape
-            closest_points_ids = np.where(np.abs(distances - closest_dist) < 0.5)[0]
-            closest_point_id = np.max(closest_points_ids) # pick most optimistic closest point
-            closest_dist = distances[closest_point_id]
-            # ass. path is evenly sampled, walk N points towards goal to find next potential waypoint
-            tentative_wpt_id = min(len(gp_fix_xy)-1, closest_point_id + self.WAYPOINT_DIST_STEPS)
-            tentative_wpt_dtg = gp_dtg[tentative_wpt_id]
-            #######
-            pub = rospy.Publisher("/tentative_wpt", Marker, queue_size=1)
-            mk = Marker()
-            mk.header.frame_id = self.kFixedFrame
-            mk.ns = "wpt"
-            mk.id = 0
-            mk.type = 1
-            mk.action = 0
-            wpt_fix_xy = gp_fix_xy[tentative_wpt_id]
-            mk.pose.position.x = wpt_fix_xy[0]
-            mk.pose.position.y = wpt_fix_xy[1]
-            mk.pose.position.z = -0.09
-            mk.scale.x = 0.1
-            mk.scale.y = 0.1 
-            mk.scale.z = 0.01
-            mk.color.b = 0
-            mk.color.g = 1
-            mk.color.r = 1
-            mk.color.a = 1
-            mk.frame_locked = True
-            pub.publish(mk)
-            #########3
-            # if waypoint is closer to goal, set as new waypoint
-            if tentative_wpt_dtg < self.waypoint_dtg:
-                self.waypoint_dtg = tentative_wpt_dtg
-                wpt_fix_xy = gp_fix_xy[tentative_wpt_id]
-                self.tf_goal_in_fix = (np.array([wpt_fix_xy[0], wpt_fix_xy[1], 0.]), # trans
-                                       tf.transformations.quaternion_from_euler(0,0,0)) # quat
-                return
-            # detect potential path failures
-            if (tentative_wpt_dtg - self.waypoint_dtg) > 5.:
-                # we seem to have moved very far backwards along the path. What happened?
-                print("Robot has veered far away ({} - {} = {} > 5) from path. Possible path switch occured?".format(
-                   tentative_wpt_dtg, self.waypoint_dtg,  tentative_wpt_dtg - self.waypoint_dtg))
-            if np.linalg.norm(rob_fix_xy - self.tf_goal_in_fix[0][:2]) < 0.5:
-                # it seems our current waypoint is no longer valid. Reset dtg
-                self.waypoint_dtg = None
+            waypoint_in_msg = np.array([
+                msg.pose.position.x,
+                msg.pose.position.y,
+            ])
+            ## msg frame to fixed frame
+            if self.kFixedFrame != msg.header.frame_id:
+                try:
+                     tf_msg_in_fix = self.tf_listener.lookupTransform(
+                             self.kFixedFrame,
+                             msg.header.frame_id,
+                             msg.header.stamp - rospy.Duration(0.1))
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+                    print("Could not find transform from waypoint frame to fixed.")
+                    print(e)
+                    return
+                waypoint_in_fix = apply_tf(waypoint_in_msg, Pose2D(tf_msg_in_fix))
+            else:
+                waypoint_in_fix = waypoint_in_msg
+            self.tf_goal_in_fix = (np.array([waypoint_in_fix[0], waypoint_in_fix[1], 0.]), # trans
+                                   tf.transformations.quaternion_from_euler(0,0,0)) # quat
 
 
     def scan_callback(self, msg):
